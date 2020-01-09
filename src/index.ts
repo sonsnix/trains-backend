7de3bde93b88ef4a6844928d66f2ed5a235e6825
Markus Sons <markus.sons@siemens.com>
2020-01-09 10:18:14 +0100

    port will be read from env variable

--- src/index.ts
+++ src/index.ts
@@ -63,8 +63,8 @@ const startServer = async () => {
 
     server.applyMiddleware({ app });
 
-    app.listen({ port: 4000 }, () =>
-        console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+    app.listen({ port: process.env.PORT || 4000 }, () =>
+        console.log(`Server ready`)
     );
 };
 
